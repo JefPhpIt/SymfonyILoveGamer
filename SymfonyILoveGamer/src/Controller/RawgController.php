a40435ce3d7b29c7d0632ef085cdd28946bae254
Alakir <lopinto.alexandre@gmail.com>
2023-11-08 13:21:49 +0100

    add pagination to my list of games

--- SymfonyILoveGamer/src/Controller/RawgController.php
+++ SymfonyILoveGamer/src/Controller/RawgController.php
@@ -3,31 +3,45 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class RawgController extends AbstractController
 {
-    #[Route('/games', name: 'rawg_games')]
-    public function getGames(HttpClientInterface $httpClient): Response
+    #[Route('/games', name: 'rawg_games', methods: ['GET'])]
+    public function getGames(Request $request, HttpClientInterface $httpClient): Response
     {
+        
+
+
         $url = $this->getParameter('rawg_api_url');
         $apiKey = $this->getParameter('rawg_api_key');
 
-        $response = $httpClient->request('GET', $url, [
+        $nextPageUrl = $request->query->get('nextPageUrl', $url); // Use the next page URL if available
+
+        $response = $httpClient->request('GET', $nextPageUrl, [
             'query' => [
-                'key' => $apiKey
+                'key' => $apiKey,
                 // ordering?
             ]
         ]);
+        
 
         $data = $response->toArray();
+
+        $games = $data['results']; // get the list of games
+
+        $previousPageURL = $data['previous']; // get the previous page URL
+        $nextPageUrl = $data['next']; // get the next page URL
 
         // dd($data);
 
         return $this->render('rawg/index.html.twig', [
-            'games' => $data['results'], // Adjust the array key to match your API response structure
+            'games' => $games,
+            'nextPageUrl' => $nextPageUrl,
+            'previousPageUrl' => $previousPageURL
         ]);
     }
 }
