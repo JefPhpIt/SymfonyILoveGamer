a75663356a2f12f0a2de9870b4308a6b18206793
Alakir <lopinto.alexandre@gmail.com>
2023-11-08 15:44:47 +0100

    basic homepage recognising user

--- SymfonyILoveGamer/src/Controller/HomeController.php
+++ SymfonyILoveGamer/src/Controller/HomeController.php
@@ -11,8 +11,15 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $user = $this->getUser();
+        // check if user is logged
+        if($user)
+        {
+            $user = $user->getUserIdentifier();
+        }
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'email' => $user,
         ]);
     }
+
 }
