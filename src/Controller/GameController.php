f8248d8534a1aaf5ca6a3f99c24760c454a5e6a1
nadyne78 <sliti.nadia13@gmail.com>
2023-11-09 15:55:33 +0100

    first commit

---
+++ src/Controller/GameController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Game;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\ResultSetMapping;
+use LDAP\Result;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class GameController extends AbstractController
+{
+    #[Route('/', name: 'app_game')]
+    public function index(Request $request, HttpClientInterface $client, ParameterBagInterface $params,EntityManagerInterface $entityManager, UserInterface $user): Response
+    {
+        // $games = $this->getUser()->getGames();
+
+        // foreach($games as $game)
+        // {
+        //     dd($game);
+        // }
+     
+        $form = $this->createFormBuilder()
+            ->add('game', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Search'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        $results = [];
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //  dd($form->getData());
+            $gameSearch = $form->getData()['game'];
+            $apiKey = $this->getParameter('app.api_rawg_key');
+            $response = $client->request("GET", "https://api.rawg.io/api/games?key=$apiKey&search=$gameSearch");
+            $results = $response->toArray()['results'];
+            //  dd($results);
+        }
+
+
+        return $this->render('game/index.html.twig', [
+            "form" => $form->createView(),
+            "results" => $results,
+
+        ]);
+    }
+    #[Route('/addgame/{id}', name: 'add_game')]
+    public function addGame($id, Request $request, HttpClientInterface $client, EntityManagerInterface $entityManager): Response
+    {
+        $apiKey = $this->getParameter('app.api_rawg_key');
+        $response = $client->request("GET", "https://api.rawg.io/api/games/$id?key=$apiKey");
+        $results = $response->toArray();
+        
+        $game=new Game();
+
+        $game->setName($results['name']);
+       
+        $game->setImagePath($results['background_image']);
+
+        $game->setDescription($results['description']);
+        $game->setIdrawgapi($results['id']);
+
+        $entityManager->persist($game);
+
+        $user = $this->getUser();
+        $user->addGame($game);
+       
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+       
+        return $this->redirectToRoute("app_game");
+
+    }
+
+    #[Route('/favorites', name:'game_favorites')]
+
+    public function favorites(): Response
+    {
+     $favorites = $this->getUser()->getGames();
+
+    return $this->render('game/favorites.html.twig', [
+        'favorites' => $favorites,]);
+    }
+}
